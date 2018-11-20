a19135b33fea8a12712d5ccc70cef36b520e49c9
nameiho.moon <nambiho@naver.com>
2018-11-21 07:39:21 +0900

    movie list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Movie from './Movie';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import './App.css';
 
 class App extends Component {
+
+  state = {};
+
+  componentDidMount () {
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={index} />
+    });
+    return movies;
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating&page=1')
+    .then(res => res.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.movies ? this._renderMovies() : <div className='circularWrap'><CircularProgress /></div>
+        }
       </div>
     );
   }
