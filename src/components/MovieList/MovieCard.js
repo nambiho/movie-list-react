f97d216060f384f23a929f5bc6545dc199b241bd
nameiho.moon <nambiho@naver.com>
2018-11-24 00:37:59 +0900

    movie list

--- src/components/MovieList/MovieCard.js
+++ src/components/MovieList/MovieCard.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography';
+import LinesEllipsis from 'react-lines-ellipsis';
 
 const styles = theme => {
 	return {
@@ -15,34 +16,56 @@ const styles = theme => {
 			flexWrap: 'wrap',
 			marginBottom: '50px',
 			textOverflow: 'ellipsis',
-			height: '90%',
+			//height: '75%',
 			boxShadow: '0 8px 38px rgba(133,133,133,0.3), 0 5px 12px rgba(133,133,133, 0.22)',
+			// [`@media screen and (min-width: 320px) and (mas-width: 667px)`]: {
+			// 	width: '100%'
+			// },
+			[`@media screen and (min-width: 320px) and (max-width: 680px) and (orientation: portrait)`]: {
+				width: '100%',
+				//flexDirection: 'column'
+			}
 		},
 		column: {
-			width: '50%',
+			width: '30%',
 			boxSizing: 'border-box',
 			textOverflow: 'ellipsis',
+			"&:last-child":{
+				width: '60%'
+			},
+			[`@media screen and (min-width: 320px) and (max-width: 680px) and (orientation: portrait)`]: {
+				width: '100%!important',
+			}
+		},
+		title: {
+			fontSize: '20px',
+			fontWeight: '600',
+			marginBottom: '20px',
 		},
 		poster: {
 			maxWidth: '100%',
 			position: 'relative',
 			marginTop: '-20%',
 			boxShadow: '-10px 19px 38px rgba(83,83,83,0.3), 10px 15px 12px rgba(80,80,80,0.22)',
+			[`@media screen and (min-width: 320px) and (max-width: 680px) and (orientation: portrait)`]: {
+				top: 0,
+				left: 0,
+				width: '100%'
+			}
 		},
 		genres: {
 			display: 'flex',
 			flexWrap: 'wrap',
 			marginBottom: '20px',
 		},
-		h1: {
-			fontSize: '20px',
-			fontWeight: '600'
+		genre: {
+			marginRight: '10px',
+			color: '#8485BD'
 		}
 	}
 }
 
 const MovieCard = ({classes, title, poster, genres, synopsis, key}) => {
-	const synopsis_size = 150;
 	return (
 		<Grid item xs={12} lg={4} sm={6} key={key}>
 			<Card classes={{ root: classes.root }}>
@@ -51,16 +74,22 @@ const MovieCard = ({classes, title, poster, genres, synopsis, key}) => {
 				</CardContent>
 
 				<CardContent className={classes.column}>
-					<Typography component='h1' variant='h5' classes={{ root: classes.genres }}>
+					<Typography component='h1' variant='h5' classes={{ root: classes.title }}>
 						{ title }
 					</Typography>
 					
 					<Typography component='p' classes={{ root: classes.genres }}>
-						{genres.map((genre, index) => <span key={index}>{ genre }</span>)}
+						{genres.map((genre, index) => <span key={index} className={classes.genre}>{ genre }</span>)}
 					</Typography>
 
-					<Typography component='p'>
-						{synopsis.length > synopsis_size ? `${synopsis.slice(0, synopsis_size)}...` : synopsis}
+					<Typography component='div'>
+						<LinesEllipsis
+							text={synopsis}
+							maxLine='3'
+							ellipsis='...'
+							trimRight
+							basedOn='letters'
+							/>
 					</Typography>
 				</CardContent>
 			</Card>
